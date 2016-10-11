730c63e9c66a5cde76eca3ddbdda9f0fbb776bd1
Jasper van den Berg <jasperberg90@gmail.com>
2016-10-11 11:35:28 +0200

    added example request in init.lua

--- esp/init.lua
+++ esp/init.lua
@@ -5,7 +5,7 @@ commun.setup("pineapple", "notanapple")
 
 base64_key = encoder.fromBase64("MJ4h1Le2SpWtbr93CuyrwQ==")
 
-test = encoder.toBase64(p_crypto.p_encrypt("Hi, I'm secret!", base64_key))
+test = encoder.toBase64(p_crypto.p_encrypt("{\"device\": {\"blood_sugars\": [{\"level\": 10}, {\"level\": 20}]}}", base64_key))
 
 --dtest = p_crypto.p_decrypt(encoder.toBase64(test), "1234567890abcdef", encoder.toBase64(p_crypto.generate_iv()))
 
