4ea06b1debf344a7933329b6d23940c1f5ec9b85
Jasper van den Berg <jasperberg90@gmail.com>
2016-10-24 12:28:21 +0200

    changed OTAUpdater to fix syntax

--- esp/OTAUpdater.lua
+++ esp/OTAUpdater.lua
@@ -4,14 +4,14 @@ ota_updater = {}
 ota_updater.write_base64_to_file = function(base64_string, filename)
     update = encoder.fromBase64(base64_string)
     
-    file.open(filename, 'w+') then
+    if file.open(filename, 'w+') then
         file.write(update)
         file.close()
     end
 end
 
 ota_updater.verify_checksum = function(file_name, encrypted_checksum)
-    file.open(filename, 'r') then
+    if file.open(filename, 'r') then
         conent = file.read()
         file.close()
     end
@@ -21,7 +21,7 @@ ota_updater.verify_checksum = function(file_name, encrypted_checksum)
     file_hash = crypto.toBase64(crypto.hash("sha1", content))
     encrypted_file_hash = p_crypto.p_encrypt(file_hash, iv)
     
-    if encrypted_file_hash == encrypted_checksum
+    if encrypted_file_hash == encrypted_checksum then
         return true
     else
         return false
@@ -32,17 +32,4 @@ ota_updater.apply_update = function(file_name)
     file.rename('init.lua', 'init.lua.old')
     file.rename(file_name, 'init.lua')
     node.restart()
-end
-
-ota_updater.check_for_update = function(server_hostname, device_id)
-    http.get('http://' .. server_hostname .. '/api/v1/firmwares',
-        'Content-Type: text/plain\r\nuid: '..deviceid..'\r\n',
-        function(code, data)
-            if (code == 204) then
-                return false
-            else
-                return data
-            end
-        end
-        )    
 end
