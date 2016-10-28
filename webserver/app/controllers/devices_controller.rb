f290a863f84c9554cc0158cac01817d4d9c5b207
Jasper van den Berg <jasperberg90@gmail.com>
2016-10-28 13:33:48 +0200

    fixed a bug that appeared when the admin checks devices

--- webserver/app/controllers/devices_controller.rb
+++ webserver/app/controllers/devices_controller.rb
@@ -2,10 +2,12 @@ class DevicesController < ApplicationController
   load_and_authorize_resource through: :current_user, singleton: true
 
   def show
-    @device = current_user.device
-    @chart_data = {}
-    @device.blood_sugars.each do |bs|
-      @chart_data[bs.created_at] = bs.level
+    unless current_user.admin?
+      @device = current_user.device
+      @chart_data = {}
+      @device.blood_sugars.each do |bs|
+        @chart_data[bs.created_at] = bs.level
+      end
     end
   end
 
