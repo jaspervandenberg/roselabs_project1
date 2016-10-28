7d57c28f6338cb3e2fd0412bddc28b29fd8a673d
Jasper van den Berg <jasperberg90@gmail.com>
2016-10-28 13:17:27 +0200

    limit bloodsugars for data platform

--- webserver/app/controllers/blood_sugars_controller.rb
+++ webserver/app/controllers/blood_sugars_controller.rb
@@ -2,7 +2,7 @@ class BloodSugarsController < ApplicationController
   skip_before_action :authenticate_user!
 
   def index
-    @blood_sugars = BloodSugar.all
+    @blood_sugars = BloodSugar.all.limit(10)
   end
 
 end
