6c98afb401d0d2bf39a2ae182b151f3a7c238888
Jasper van den Berg <jasperberg90@gmail.com>
2016-10-28 18:29:51 +0200

    do not throw an exception on bad decryption, this prevents a padding oracle attack

--- webserver/app/controllers/api/v1/devices_controller.rb
+++ webserver/app/controllers/api/v1/devices_controller.rb
@@ -7,10 +7,13 @@ class Api::V1::DevicesController < Api::V1::ApplicationController
       # Fake decryption to prevent timing attacks
       decrypt_body(request.body.string, 'thisisafdflkjwflkwejlfkakeiv', Device.first)
     else
-      body = decrypt_body(request.body.string, request.headers['iv'], @device)
-      json_body = JSON.parse(body.strip)['device']['blood_sugars']
-      json_body.each do |sugar_level|
-        @device.blood_sugars.create(level: sugar_level['level'])
+      begin
+        body = decrypt_body(request.body.string, request.headers['iv'], @device)
+        json_body = JSON.parse(body.strip)['device']['blood_sugars']
+        json_body.each do |sugar_level|
+          @device.blood_sugars.create(level: sugar_level['level'])
+        end
+      rescue OpenSSL::Cipher::CipherError
       end
     end
 
